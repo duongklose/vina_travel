842949bb37c6fb37b3cc987fad04536b12011004
duongklose <duongklose@gmail>
2021-05-03 18:11:58 +0700

    add tablayout in manageTicket

---
+++ app/src/main/java/com/example/vinatravel/ui/home/manage_ticket/ManageTicketAdapter.java
@@ -0,0 +1,53 @@
+package com.example.vinatravel.ui.home.manage_ticket;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class ManageTicketAdapter extends FragmentPagerAdapter {
+    public ManageTicketAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new MyTicketFragment();
+            case 1:
+                return new OldTicketFragment();
+            case 2:
+                return new CancelledTicketFragment();
+            default:
+                return new MyTicketFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        String title = "";
+        switch (position){
+            case 0:
+                title = "Vé đang đặt";
+                break;
+            case 1:
+                title = "Chuyến đã đi";
+                break;
+            case 2:
+                title = "Vé đã hủy";
+                break;
+            default:
+                title = "Vé đang đặt";
+        }
+        return title;
+    }
+}
--- app/src/main/java/com/example/vinatravel/ui/home/manage_ticket/TicketFragment.java
+++ app/src/main/java/com/example/vinatravel/ui/home/manage_ticket/TicketFragment.java
@@ -3,64 +3,43 @@ package com.example.vinatravel.ui.home.manage_ticket;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.vinatravel.R;
+import com.google.android.material.tabs.TabLayout;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TicketFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class TicketFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    TabLayout tabLayout;
+    ViewPager viewPager;
 
     public TicketFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TicketFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TicketFragment newInstance(String param1, String param2) {
-        TicketFragment fragment = new TicketFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ticket, container, false);
+        View view = inflater.inflate(R.layout.fragment_ticket, container, false);
+
+        tabLayout = view.findViewById(R.id.tab_layout_ticket);
+        viewPager = view.findViewById(R.id.view_pager_ticket);
+
+        ManageTicketAdapter manageTicketAdapter = new ManageTicketAdapter(getFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        viewPager.setAdapter(manageTicketAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+        return view;
     }
 }
