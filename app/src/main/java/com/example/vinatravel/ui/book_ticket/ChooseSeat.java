e2d0d5cf870df44f8d77c2995ca320982b5f3c0b
duongklose <duongklose@gmail>
2021-05-09 00:54:17 +0700

    add 40seats fragment into choose seat screen

--- app/src/main/java/com/example/vinatravel/ui/book_ticket/ChooseSeat.java
+++ app/src/main/java/com/example/vinatravel/ui/book_ticket/ChooseSeat.java
@@ -5,17 +5,33 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.vinatravel.R;
+import com.google.android.material.appbar.MaterialToolbar;
 
-public class ChooseSeat extends Activity {
+public class ChooseSeat extends AppCompatActivity {
+    MaterialToolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.choose_seat_screen);
+        toolbar = findViewById(R.id.topAppBarChooseSeat);
+        toolbar.setTitle("Chọn ghế");
+
         Intent intent = getIntent();
         String name = intent.getStringExtra("chooseSeat");
         Log.d("AAA", String.valueOf(name));
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment, new SeatFragment()).addToBackStack(null).commit();
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/vinatravel/ui/book_ticket/SeatFragment.java
+++ app/src/main/java/com/example/vinatravel/ui/book_ticket/SeatFragment.java
@@ -26,6 +26,8 @@ public class SeatFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_seat, container, false);
+
+        View view = inflater.inflate(R.layout.item_40_seats_coach, container, false);
+        return view;
     }
 }
