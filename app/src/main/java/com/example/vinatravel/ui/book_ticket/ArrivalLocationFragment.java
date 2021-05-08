6409990c993753576a9c9d5080b9df6779d5db7f
duongklose <duongklose@gmail>
2021-05-09 03:14:25 +0700

    add choose arrival location screen

---
+++ app/src/main/java/com/example/vinatravel/ui/book_ticket/ArrivalLocationFragment.java
@@ -0,0 +1,30 @@
+package com.example.vinatravel.ui.book_ticket;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.vinatravel.R;
+
+public class ArrivalLocationFragment extends Fragment {
+
+    public ArrivalLocationFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_arrival_location, container, false);
+        return view;
+    }
+}
--- app/src/main/java/com/example/vinatravel/ui/book_ticket/ChooseDepartureLocation.java
+++ app/src/main/java/com/example/vinatravel/ui/book_ticket/ChooseDepartureLocation.java
@@ -5,18 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.vinatravel.R;
 import com.google.android.material.appbar.MaterialToolbar;
 
 public class ChooseDepartureLocation extends AppCompatActivity {
     MaterialToolbar toolbar;
+    Button btnContinue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.choose_departure_location_screen);
         toolbar = findViewById(R.id.topAppBarChooseDepartureLocation);
+        btnContinue = findViewById(R.id.continue_btn_departure_location);
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fl_content, new DepartureLocationFragment()).addToBackStack(null).commit();
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
@@ -27,5 +30,12 @@ public class ChooseDepartureLocation extends AppCompatActivity {
         });
 
         Intent intent = getIntent();
+        btnContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(getApplicationContext(), ChooseArrivalLocation.class);
+                startActivity(intent1);
+            }
+        });
     }
 }
